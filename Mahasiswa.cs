62129853d4d46a268ebdaa935eacf656047a4ea2
Rakunberacun <mrakan711@gmail.com>
2025-04-27 00:16:19 +0700

    menambahkan API mahasiswa

---
+++ Mahasiswa.cs
@@ -0,0 +1,8 @@
+namespace tpmodul10_103022330044
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
--- obj/Debug/net8.0/tpmodul10_103022330044.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul10_103022330044.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul10_103022330044")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff65cd695b460560849282240d8ccb82c9a768d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul10_103022330044")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul10_103022330044")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
